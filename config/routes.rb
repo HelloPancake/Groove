0e665404f891b764e5cb38480a2039ed7d249cd1
Richard <emailrchen@gmail.com>
2019-03-26 16:56:21 -0400

    First Heroku Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: [:show, :create]
+    resources :session, only: [:create, :destroy]
+    resources :posts, only: [:show, :create, :edit, :destroy]
+    resources :notes, only: [:show, :create, :destroy, :update]
+    resources :likes, only: [:create, :destroy]
+    resources :follows, only: [:create, :destroy]
+  end
+
+
+
+  root "static_pages#root"
+
 end
