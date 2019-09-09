43785202fc8e1e17607a0724b7756094beaff814
Richard <emailrchen@gmail.com>
2019-09-09 14:36:00 -0400

    css

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -9,7 +9,7 @@ const usersReducer = (state = {}, action) => {
     Object.freeze(state);
     switch (action.type) {
         case RECEIVE_CURRENT_USER:
-            debugger
+            
             return merge({}, state, { [action.currentUser.users.id]: action.currentUser.users });
         case RECEIVE_ALL_POSTS:
             return merge({}, state, action.users)
